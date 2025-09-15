763a3bb36a8e9b157402d23adde83d260e6d1f05
Alecm37 <145499415+Alecm37@users.noreply.github.com>
2025-09-15 18:35:26 -0400

    Add files via upload
    Very basic raytracing demo. Includes reflection, refraction, and diffuse lighting.

--- frag.js
+++ frag.js
@@ -12,11 +12,13 @@ uniform sampler2D uSky;
 uniform sampler2D uBrick; 
 
 #define FOCAL_LEN 2.0 //length between camera and pixels 
-#define SPHERE_CENTER vec3(0.0, -0.25, 10.0)
+#define SPHERE_CENTER vec3(0.0, -2., 12.0)
 #define SPHERE_RADIUS 0.75
 #define PLANE_CENTER vec4(0.0, 1.0, 0.0, 1.0)
 #define PLANE_OFFSET vec3(0.0, 0.0, 10.0)
 #define PI 3.14159265
+#define MAX_BOUNCES 8
+#define EPS 1e-4
 
 //ro = ray origin, rd = ray direction, ce = center pos circle, ra= radius 
 vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra, in vec3 sphOffset) //sphere intersection function, returns distance between first and second intersections, -1 if missed entirely (no intersection)
@@ -29,6 +31,8 @@ vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra, in vec3 sphOffs
     h = sqrt( h );
     return vec2( -b-h, -b+h ); //intersection in 2D space 
 }
+
+
 
 float plaIntersect(in vec3 ro, in vec3 rd, in vec4 p, in vec3 plaOffset)
 {
@@ -45,80 +49,6 @@ float plaIntersect(in vec3 ro, in vec3 rd, in vec4 p, in vec3 plaOffset)
     } else {
         return -1.0;
     }
-}
-
-float torIntersect( in vec3 ro, in vec3 rd, in vec2 tor, in vec3 torOffset )
-{
-    ro -= torOffset;
-    float po = 1.0;
-    float Ra2 = tor.x*tor.x;
-    float ra2 = tor.y*tor.y;
-    float m = dot(ro,ro);
-    float n = dot(ro,rd);
-    float k = (m + Ra2 - ra2)/2.0;
-    float k3 = n;
-    float k2 = n*n - Ra2*dot(rd.xy,rd.xy) + k;
-    float k1 = n*k - Ra2*dot(rd.xy,ro.xy);
-    float k0 = k*k - Ra2*dot(ro.xy,ro.xy);
-    
-    if( abs(k3*(k3*k3-k2)+k1) < 0.01 )
-    {
-        po = -1.0;
-        float tmp=k1; k1=k3; k3=tmp;
-        k0 = 1.0/k0;
-        k1 = k1*k0;
-        k2 = k2*k0;
-        k3 = k3*k0;
-    }
-    
-    float c2 = k2*2.0 - 3.0*k3*k3;
-    float c1 = k3*(k3*k3-k2)+k1;
-    float c0 = k3*(k3*(c2+2.0*k2)-8.0*k1)+4.0*k0;
-    c2 /= 3.0;
-    c1 *= 2.0;
-    c0 /= 3.0;
-    float Q = c2*c2 + c0;
-    float R = c2*c2*c2 - 3.0*c2*c0 + c1*c1;
-    float h = R*R - Q*Q*Q;
-    
-    if( h>=0.0 )  
-    {
-        h = sqrt(h);
-        float v = sign(R+h)*pow(abs(R+h),1.0/3.0); // cube root
-        float u = sign(R-h)*pow(abs(R-h),1.0/3.0); // cube root
-        vec2 s = vec2( (v+u)+4.0*c2, (v-u)*sqrt(3.0));
-        float y = sqrt(0.5*(length(s)+s.x));
-        float x = 0.5*s.y/y;
-        float r = 2.0*c1/(x*x+y*y);
-        float t1 =  x - r - k3; t1 = (po<0.0)?2.0/t1:t1;
-        float t2 = -x - r - k3; t2 = (po<0.0)?2.0/t2:t2;
-        float t = 1e20;
-        if( t1>0.0 ) t=t1;
-        if( t2>0.0 ) t=min(t,t2);
-        return t;
-    }
-    
-    float sQ = sqrt(Q);
-    float w = sQ*cos( acos(-R/(sQ*Q)) / 3.0 );
-    float d2 = -(w+c2); if( d2<0.0 ) return -1.0;
-    float d1 = sqrt(d2);
-    float h1 = sqrt(w - 2.0*c2 + c1/d1);
-    float h2 = sqrt(w - 2.0*c2 - c1/d1);
-    float t1 = -d1 - h1 - k3; t1 = (po<0.0)?2.0/t1:t1;
-    float t2 = -d1 + h1 - k3; t2 = (po<0.0)?2.0/t2:t2;
-    float t3 =  d1 - h2 - k3; t3 = (po<0.0)?2.0/t3:t3;
-    float t4 =  d1 + h2 - k3; t4 = (po<0.0)?2.0/t4:t4;
-    float t = 1e20;
-    if( t1>0.0 ) t=t1;
-    if( t2>0.0 ) t=min(t,t2);
-    if( t3>0.0 ) t=min(t,t3);
-    if( t4>0.0 ) t=min(t,t4);
-    return t;
-}
-
-vec3 torNormal( in vec3 pos, vec2 tor)
-{
-    return normalize( pos*(dot(pos,pos)-tor.y*tor.y - tor.x*tor.x*vec3(1.0,1.0,-1.0)));
 }
 
 // axis aligned box centered at the origin, with size boxSize
@@ -152,12 +82,13 @@ vec3 sampleHdri(vec3 d){ //input ray direction
 
 struct Surface{
     bool hit; //did it hit? yes or no (intersection)
+    bool exit; 
     vec3 position; //vector pointing to intersection
     vec3 normal; //normalize the position of point intersected 
     vec3 albedo; //base color of object 
     vec3 emission; //emissive color of object 
     float ior; //index of refraction 
-    float transp; //transparency (1 is fully transparent, 0 is fully opaque)
+    bool std; 
 };
 
 Surface shootRay(vec3 ro, vec3 rd){
@@ -172,10 +103,10 @@ Surface shootRay(vec3 ro, vec3 rd){
         surface.hit = true; 
         surface.position = ro + rd * sphere; 
         surface.normal = normalize(surface.position - SPHERE_CENTER - sphereOffset);
-        surface.albedo = vec3(0.5, 0.5, 0.5); 
-        surface.emission = vec3(0.);
-        surface.ior = 3.;
-        surface.transp = 1.; 
+        surface.albedo = vec3(0.1, 0.4, 0.9); 
+        surface.emission = vec3(0.2);
+        surface.ior = 1.5;
+        surface.std = false; 
     }
     vec3 plaOffset = vec3(0.);
     float plane = plaIntersect(ro, rd, PLANE_CENTER, plaOffset);
@@ -186,25 +117,11 @@ Surface shootRay(vec3 ro, vec3 rd){
         surface.normal = PLANE_CENTER.xyz - plaOffset; 
         surface.albedo = vec3(0.4, 0.4, 0.4);
         surface.emission = vec3(0.);
-        surface.ior = 1.45;
-        surface.transp = 1.; 
-    }
-    vec2 torVec = vec2(0.5, 0.25);
-    vec3 torOffset = vec3(0., 0., 7.0);
-    float tor = torIntersect(ro, rd, torVec, torOffset);
-    if (tor > 0. && tor < tMin){
-        tMin = tor; 
-        surface.hit = true; 
-        surface.position = ro+rd*tor; 
-        surface.normal = torNormal(surface.position - torOffset, torVec);
-        surface.albedo = vec3(0.5, 0.5, 0.5);
-        surface.emission = vec3(0.);
-        surface.ior = 3.; 
-        surface.transp = 1.;
+        surface.ior = 1.;
+        surface.std = false; 
     }
     vec3 boxSize = vec3(0.5);
     vec3 boxNormal = vec3(0.);
-    //vec3 boxPos  = vec3(2.0, -0.5, 10.0);
     vec3 boxOffset = vec3(0., 0., 8.);
     float box = boxIntersection(ro, rd, boxSize, boxNormal, boxOffset).x;
     if (box > 0. && box < tMin){
@@ -212,10 +129,24 @@ Surface shootRay(vec3 ro, vec3 rd){
         surface.hit = true; 
         surface.position = ro+rd*box; 
         surface.normal = boxNormal;
-        surface.albedo = vec3(0.5, 0.5, 0.5);
+        surface.albedo = vec3(0.4, 0.4, 0.4);
         surface.emission = vec3(0.);
-        surface.ior = 3.; 
-        surface.transp = 0.5;
+        surface.ior = 1.5; 
+        surface.std = false; 
+    }
+    vec3 lightBoxSize = vec3(0.5);
+    vec3 lightBoxNormal = vec3(0.);
+    vec3 lightBoxOffset = vec3(3., 1., 10.);
+    float lightBox = boxIntersection(ro, rd, lightBoxSize, lightBoxNormal, lightBoxOffset).x;
+    if (lightBox > 0. && lightBox < tMin){
+        tMin = lightBox; 
+        surface.hit = true; 
+        surface.position = ro+rd*lightBox; 
+        surface.normal = lightBoxNormal;
+        surface.albedo = vec3(0.8, 0.8, 0.8);
+        surface.emission = vec3(10.);
+        surface.ior = 1.5; 
+        surface.std = false; 
     }
     return surface;
 }
@@ -252,8 +183,11 @@ vec3 sampleHemiSphere(vec3 n){//input surface.normal, output vector representing
 }
 
 float fresnel(float costheta, float eta){ //input costheta of incoming ray and normal, ratio between current ior and future ior 
+    //fixing ior to scale correctly using (abriged) inverse sigmoid function 
+    //n(scaled) = -ln((1/x)-1)+1
+   
     float c = abs(costheta);
-    float g = eta * eta - 1. + c * c; //snell's law, if TIR occurs 
+    float g = eta * eta * 2. - 1. + c * c; //snell's law, if TIR occurs 
     if(g > 0.){ //fresnel equations 
         g = sqrt(g);
         float A = (g - c) / (g + c);
@@ -264,11 +198,6 @@ float fresnel(float costheta, float eta){ //input costheta of incoming ray and n
     }
 }
 
-//glass 
-
-//fog 
-
-//maybe port to C++??
 
 
 void main(){
@@ -279,36 +208,58 @@ void main(){
     vec3 pixelPos = vec3(uv, FOCAL_LEN); //pixel position from uv and FOCAL_LEN (z)
     vec3 rayDirection = normalize(pixelPos); //return ray direction 
 
-    rayDirection = mat3(uView) * rayDirection;
+    rayDirection = mat3(uView) * rayDirection; //standard ray direction
     vec3 rayOrigin = uView[3].xyz;
-
+    
     vec3 radiance = vec3(0.);
     vec3 throughput = vec3(1.);
 
-    for (int i = 0; i < 8; i++){
+
+
+   
+   
+    //reflections and refractions 
+    for (int i = 0; i < MAX_BOUNCES; i++){
         Surface surface = shootRay(rayOrigin, rayDirection);
+        float ior = surface.ior;
+        float eta = (dot(rayDirection, surface.normal) < 0.0) ? 1./ior : ior;  
+        vec3 N = (dot(rayDirection, surface.normal) < 0.0) ? surface.normal : -surface.normal;
+
         if(!surface.hit){
             radiance+=throughput * sampleHdri(rayDirection); 
             break; 
         }
-        vec3 wo; 
-        float fresnel = fresnel(dot(rayDirection, surface.normal), surface.ior);
-        bool isReflection = false; 
-        if(pickRandom() < fresnel){
-            wo = reflect(rayDirection, surface.normal);
-            isReflection = true; 
-        } else {
-            wo = sampleHemiSphere(surface.normal);
+        if(surface.std){
+            radiance = surface.albedo; 
+            break;
         }
+        vec3 wo; 
+        float fresnelRefl = fresnel(dot(rayDirection, N), ior);
+        bool isReflection = false; 
+        if(pickRandom() < fresnelRefl){
+            if(pickRandom()>.5){
+            wo = reflect(rayDirection, N);
+            isReflection = true;
+            }
+            else{
+            wo = sampleHemiSphere(N);
+            }
+        } 
+        else {
+            if(pickRandom()>.5){
+            wo = refract(rayDirection, N, eta);
+            isReflection = true;
+            }
+            else{
+            wo = sampleHemiSphere(N);
+            }
+        }
+        radiance += surface.emission * throughput;
+        throughput *= ((isReflection)? vec3(1.) : surface.albedo);
 
-
-        radiance += throughput * surface.emission; 
-        throughput *= (isReflection)? vec3(1.) : surface.albedo;
-
-        rayOrigin = surface.position + surface.normal * 0.0001; 
-        //rayDirection = reflect(rayDirection, surface.normal);
+        rayOrigin = surface.position + normalize(wo) * EPS; 
         rayDirection = wo;
-    }
+    }  
 
     vec3 lastFrame = texture(uLastFrame, gl_FragCoord.xy/uResolution).rgb; //averages each frame 
     fragColor.rgb = (radiance + lastFrame*uFrame)/(uFrame+1.);
